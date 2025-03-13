237c2ff06aa2f21cf2055caf266a503b596e663c
chiji1108 <shingo.pad@gmail.com>
2025-03-13 23:10:38 +0900

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by 千々岩真吾 on 2025/03/13.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
